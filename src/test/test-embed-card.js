cbe5fa5b1993e36857549a967e7676b6a91d28ba
Anthony Dillon <me@anthonydillon.com>
2015-11-11 15:43:20 +0000

    Strip out test

--- src/test/test-embed-card.js
+++ src/test/test-embed-card.js
@@ -1,7 +1,6 @@
 var assert = require('assert');
 describe('Card', function() {
   it('detect single juju-card', function () {
-    assert.equal(-1, [1,2,3].indexOf(5));
-    assert.equal(-1, [1,2,3].indexOf(0));
+    
   });
 });
