e89eea076a57a3e097345143460e35eac1666fc4
Anthony Dillon <me@anthonydillon.com>
2016-03-11 15:11:14 +0000

    Add copy to clipboard and some reset styles

--- src/js/juju-cards-v1.0.0.js
+++ src/js/juju-cards-v1.0.0.js
@@ -7,6 +7,10 @@ let jujuCards = () => {
                     '&include=id-series' +
                     '&include=extra-info' +
                     '&include=promulgated';
+  let copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">' +
+  '<path d="M4.85 11.15l6.3-6.3" overflow="visible" fill="none" stroke="gray" stroke-width="1.463"/>' +
+  '<path d="M12.152 0a1.984 1.984 0 0 0-.33.04c-.867.173-1.42.78-1.898 1.26L8.487 2.738c-.48.48-1.087 1.03-1.26 1.898-.075.37-.044.76.096 1.167l1.77-1.77c.102-.113.215-.23.337-.352l1.437-1.435c.48-.48.886-.83 1.217-.896a.877.877 0 0 1 .277-.01c.313.034.738.245 1.398.905.88.88.96 1.342.895 1.673-.066.33-.417.736-.896 1.216L12.32 6.57c-.124.123-.242.237-.356.34l-1.767 1.77c.407.14.797.17 1.168.096.867-.173 1.42-.78 1.898-1.26l1.437-1.44c.48-.48 1.087-1.03 1.26-1.898.174-.867-.223-1.84-1.26-2.878C13.923.522 13.18.105 12.492.017a2.125 2.125 0 0 0-.34-.016zM4.965 7.19a1.984 1.984 0 0 0-.33.038c-.867.174-1.42.782-1.898 1.26L1.3 9.925c-.48.48-1.087 1.03-1.26 1.898-.174.868.223 1.842 1.26 2.88 1.037 1.035 2.01 1.432 2.878 1.26.867-.175 1.42-.783 1.898-1.262l1.437-1.436c.48-.48 1.087-1.03 1.26-1.898.075-.37.044-.76-.096-1.168l-1.765 1.765c-.104.114-.218.233-.342.357l-1.437 1.437c-.48.48-.886.83-1.217.895-.33.066-.793-.016-1.673-.895-.88-.88-.96-1.343-.895-1.673.066-.33.417-.737.896-1.216L3.68 9.43c.124-.122.24-.236.355-.34l1.767-1.767a2.498 2.498 0 0 0-.497-.12 2.125 2.125 0 0 0-.34-.015z" fill="gray"/>' +
+'</svg>';
 
 
   let cards = document.querySelectorAll('.' + targetClass);
@@ -53,9 +57,6 @@ let jujuCards = () => {
     }
     let addLink = `https://demo.jujucharms.com/?deploy-target=${getImageID(id)}`;
 
-    console.log(name + ' | ' + series + ' | ' + revision);
-    console.log(detailsLink);
-
     let dom = `<div class="juju-card__container bundle-card">` +
         `<a href="${detailsLink}" class="bundle-card__link">View details</a>` +
         `<header class="bundle-card__header">` +
@@ -71,7 +72,10 @@ let jujuCards = () => {
           `<ul class="bundle-card__actions">` +
             `<li class="bundle-card__actions-item--details">` +
               `<label class="bundle-card__actions-label" for="cli-deploy">Deploy with the CLI:</label>` +
-              `<input class="bundle-card__actions-field" readonly="readonly" value="juju deploy ${id}" onclick="this.select();" id="cli-deploy">` +
+              `<div class="bundle-card__actions-command">` +
+                `<input class="bundle-card__actions-field js-clipboard-input" readonly="readonly" value="juju deploy ${id}" onclick="this.select();" id="cli-deploy">` +
+                `<button class="bundle-card__actions-copy-to-clipboard js-copy-to-clipboard" onclick="jujuCards.copyToClipboard(this.parentElement);" title="Copy to clipboard">${copyIcon}</button>` +
+              `</div>` +
             `</li>` +
             `<li class="bundle-card__actions-item--demo">` +
               `<a href="${addLink}" class="bundle-card__add-button--primary">Deploy with Juju</a>` +
@@ -123,7 +127,10 @@ let jujuCards = () => {
           `<ul class="charm-card__actions">` +
           `<li class="charm-card__actions-item--details">` +
             `<label class="charm-card__actions-label" for="cli-deploy">Deploy with the CLI:</label>` +
-            `<input class="charm-card__actions-field" readonly="readonly" value="juju deploy ${id}" onclick="this.select();" id="cli-deploy">` +
+            `<div class="charm-card__actions-command">` +
+              `<input class="charm-card__actions-field js-clipboard-input" readonly="readonly" value="juju deploy ${id}" onclick="this.select();" id="cli-deploy">` +
+              `<button class="charm-card__actions-copy-to-clipboard js-copy-to-clipboard" onclick="jujuCards.copyToClipboard(this);" title="Copy to clipboard">${copyIcon}</button>` +
+            `</div>` +
           `</li>` +
             `<li class="charm-card__actions-item--demo">` +
               `<a href="${addLink}" class="charm-card__add-button--primary">Deploy with Juju</a>` +
@@ -223,6 +230,18 @@ jujuCards.updateHead = () => {
   css.href = `${filePath}.css`;
   css.media = 'all';
   document.getElementsByTagName('head')[0].appendChild(css);
+}
+
+jujuCards.copyToClipboard = function (item) {
+  let commandInput = item.parentElement.getElementsByTagName('input')[0];
+  if (commandInput) {
+    commandInput.select();
+    try {
+      document.execCommand('copy');
+    } catch(err) {
+      console.log('Unable to copy');
+    }
+  }
 }
 
 if (window.onload && typeof window.onload === 'function') {
